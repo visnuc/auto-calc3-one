0255f044b8bab2f7de8c8e5f0287b0605b81dc9d
pr3370m <pr3370m@gmail.com>
2018-07-29 21:16:21 +0600

    committing looping mechanism and syncing

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 def main():
     your_name = input("Enter your name: ")
     print("Hello, ", your_name + "!")
-    l_resp = input("Are you done? [y/n]: ")
-    if l_resp == "y":
-        exit()
-    else:
+    user_resp = input("Are you done? [y/n]: ")
+    user_resp_l = user_resp.lower()
+    if user_resp_l == "n":
         main()
-
-
+    else:
+        exit()
 
 if __name__ == "__main__":
     main()
